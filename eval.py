e0d8e527c6b2d5df5495609bb716fcd943be31dd
horologium <official.horologium@gmail.com>
2021-09-16 10:52:27 -0500

    added TODO

--- eval.py
+++ eval.py
@@ -1,5 +1,6 @@
 import pytest as pt
 import os
+#Grading structure based on https://github.com/The-College-of-the-Ozarks/2021DATASTRUCTURES/blob/main/README.md
 
 
 
@@ -8,8 +9,12 @@ import os
 
 
 def main():
-    
-    
+    #Clone repos to a temp folder
+    #TODO
+    #Copy student folders into an appdata folder
+    #TODO
+    #For each student's assignment files, run pytest and grade?
+    #TODO
 
 
 
