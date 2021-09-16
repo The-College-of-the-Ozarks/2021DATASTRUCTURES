a4ced95b9eff2d52b24002d4cdb5b09d52fef605
Murphy <226434@student.cofo.edu>
2021-09-16 14:28:14 -0500

    adding cloning code

--- eval.py
+++ eval.py
@@ -1,16 +1,27 @@
 import pytest as pt
 import os
+import git
 #Grading structure based on https://github.com/The-College-of-the-Ozarks/2021DATASTRUCTURES/blob/main/README.md
 
 
-
+def cloneBuilder(rootRepo):
+    fNames = []
+    sfh = "https://github.com/"
+    igns = open('usernames.txt', 'r')
+    for name in igns.readAllLines():
+        fName.append(name)
+    return fNames
 
 
 
 
 def main():
     #Clone repos to a temp folder
-    #TODO
+    repoName = input('Please enter the name of the repository that will be cloned from the students (case sensitive)')
+    studentReopList = cloneBuilder(repoName)
+    for student in studentRepoList:
+        os.mkdir(os.getcwd() + student)
+        
     #Copy student folders into an appdata folder
     #TODO
     #For each student's assignment files, run pytest and grade?
