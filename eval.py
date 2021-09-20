b376919dfb7161a827a07fb990ca79c2ebba8c79
H0rologium <33498180+H0rologium@users.noreply.github.com>
2021-09-20 09:51:01 -0500

    adding clone code

--- eval.py
+++ eval.py
@@ -8,9 +8,12 @@ def cloneBuilder(rootRepo):
     fNames = []
     sfh = "https://github.com/"
     igns = open('usernames.txt', 'r')
-    for name in igns.readAllLines():
-        fName.append(name)
-    return fNames
+    people = []
+    for name in igns:
+        fNames.append(sfh + name + '/' + rootRepo + '.git')
+        people.append(name)
+    igns.close()
+    return [fNames, people]
 
 
 
@@ -18,12 +21,24 @@ def cloneBuilder(rootRepo):
 def main():
     #Clone repos to a temp folder
     repoName = input('Please enter the name of the repository that will be cloned from the students (case sensitive)')
+    studentRepoList = cloneBuilder(repoName)
+    print(studentRepoList[1])
+    #Make the root dirs to clone into
+    for student in studentRepoList[1]:
+        if '#' not in student:
+            os.mkdir(os.getcwd() + '/' + str(student))
+            print('Made a folder for ' + student)
+            #Clone each student's repo
+            print('Cloning ' + repoName + ' for ' + student)
+            cloned_repo = repo.clone(os.path.join(rw_dir, '')
+
     studentReopList = cloneBuilder(repoName)
     for student in studentRepoList:
         os.mkdir(os.getcwd() + student)
         os.chdir(os.getcwd() + student)
         
     #Copy student folders into an appdata folder
+            
     #TODO
     #For each student's assignment files, run pytest and grade?
     #TODO
